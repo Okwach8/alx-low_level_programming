a5964a8a42212d6e6cb93968a358641bfd977c65
Okwach8 <westher188@gmail.com>
2022-04-08 17:10:28 +0300

    more malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,245 +1,197 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-* printError - prints error message
-* Return: nothing
-**/
-void printError(void)
+ * _prt - print string followed by newline
+ * @s: string to print
+ */
+void _prt(char *s)
 {
-	_putchar('E');
-	_putchar('r');
-	_putchar('r');
-	_putchar('o');
-	_putchar('r');
+	while (*s != '\0')
+		_putchar(*s++);
 	_putchar('\n');
 }
 /**
-* digit_checker - checks if the passed arguements are valid
-* @str: pointer to char
-* Return: 0 if all chars are digits else -1
-**/
-int digit_checker(char *str)
+ * _realloc - Re-allocate memory for a larger or smaller size
+ * @ptr: Pointer to the old memory block
+ * @old_size: The old size of the memory block
+ * @new_size: The new size of the memory block being created
+ *
+ * Return: Pointer to new memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	int i;
+	void *space;
+	char *spacecpy, *ptrcpy;
+	unsigned int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	if (new_size == 0 && ptr != NULL)
 	{
-		if (str[i] < '0' || str[i] > '9')
-		{
-			printError();
-			return (-1);
-		}
+		free(ptr);
+		return (NULL);
 	}
+	if (new_size == old_size)
+		return (ptr);
+	/* regardless, we need to make new space of new_size */
+	space = malloc(new_size);
+	if (space == NULL)
+		return (NULL);
+	/* if ptr is null, return space without copying */
+	if (ptr == NULL)
+		return (space);
+	/* copy old contents into new space */
+	spacecpy = space;
+	ptrcpy = ptr;
+	for (i = 0; i < old_size && i < new_size; i++)
+		spacecpy[i] = ptrcpy[i];
+	free(ptr);
+	return (space);
+}
+/**
+ * _calloc - Allocate memory and initalize space to zero
+ * @nmemb: number of elements
+ * @size: size of bytes
+ *
+ * Return: pointer to memory space, or NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *space;
+	char *memset;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	space = malloc(nmemb * size);
+	if (space == NULL)
+		return (NULL);
+
+	memset = space;
+	for (i = 0 ; i < nmemb * size; i++)
+	{
+		*(memset + i) = 0;
+	}
+
+	return (space);
+}
+/**
+ * _notdigit - check to see if string is only digits
+ * @s: string to check
+ *
+ * Return: 0 if only digits, 1 if non digit chars
+ */
+int _notdigit(char *s)
+{
+	for ( ; *s; s++)
+		if (*s < '0' || *s > '9')
+			return (1);
 	return (0);
 }
 /**
-* _strlen - calculates length of passed string
-* @str: pointer to char
-* Return: length of string
-**/
-int _strlen(char *str)
+ * rev_ - Reverse a string in place
+ * @s: string to reverse
+ */
+void rev_(char *s)
+{
+	char tmp;
+	int i, j;
+
+	for (i = 0; s[i]; i++)
+		;
+	i--;
+	for (j = 0; j <= i / 2; j++)
+	{
+		tmp = s[j];
+		s[j] = s[i - j];
+		s[i - j] = tmp;
+	}
+}
+/**
+ * _addup - add up integer array
+ * @arr: array to count
+ * @n: number of ints to count
+ * @place: which tens place to count
+ *
+ * Return: result of addition
+ */
+int _addup(int *arr, int n, int place)
+{
+	int sum, i;
+
+	for (i = 0, sum = 0; i < n; i++)
+	{
+		sum += arr[n * i + place];
+	}
+	return (sum);
+}
+/**
+ * cut_zeros - cut off my zeros
+ * @s: string to cut
+ *
+ * Return: length of s
+ */
+int cut_zeros(char *s)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-		;
+	i = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	i--;
+	s--;
+	while (*s == '0' && i > 0)
+	{
+		*s = '\0';
+		s--;
+		i--;
+	}
 	return (i);
 }
-/**
-* _reallocate - reallocates memory if carry over if greater than 9
-* @str: pointer to char
-* @newSize: new size to allocate
-* Return: pointer to newly allocated memory
-**/
-char *_reallocate(char *str, int newSize)
-{
-	char *newStr;
 
-	newStr = malloc(newSize * sizeof(char *));
-	if (newStr == NULL)
-	{
-		free(str);
-		return (NULL);
-	}
-	return (newStr);
-}
 /**
-* mulNums - multiply an array
-* @str: array to be multiplied
-* @n: multiplier
-* @zeros: number of zeros to be added
-* Return: array with result
-**/
-char *mulNums(char *str, char n, int zeros)
-{
-	char *res, *newRes;
-	int i, k, mul, size, len, reSize;
-
-	len = _strlen(str);
-	size = reSize = len + zeros + 1;
-	res = malloc(sizeof(char) * size);
-	if (res == NULL)
-	{
-		printError();
-		return (NULL);
-	}
-	size--;
-	while (zeros > 0)
-	{
-		res[size] = '0';
-		size--, zeros--;
-	}
-	k = 0;
-	mul = 1;
-	while (size > 0)
-	{
-		mul = ((str[len - 1] - '0') * (n - '0')) + k;
-		res[size] = (mul % 10) + '0';
-		k = mul / 10;
-		size--, len--;
-	}
-	if (k >= 0 && k <= 9)
-	{
-		res[size] = k + '0';
-		return (res);
-	}
-	else if (k > 9)
-	{
-		res[size] = (k % 10) + '0';
-		newRes = _reallocate(res, reSize + 1);
-		if (newRes == NULL)
-			return (NULL);
-		newRes[0] = (k / 10) + '0';
-		for (i = 1; i < reSize + 1; i++)
-			newRes[i] = res[i];
-		free(res);
-		return (newRes);
-	}
-	res[size] = '0';
-	return (res);
-}
-/**
-* addNums - add two arrays of digits
-* @str1: array 1
-* @str2: array2
-* Return: pointer to char with sum
-**/
-char *addNums(char *str1, char *str2)
-{
-	char *add, *newAdd;
-	int sum, k, len1, len2, size, reSize, i;
-
-	len1 = _strlen(str1);
-	len2 = _strlen(str2);
-	size = reSize = len2 + 2;
-	add = malloc(sizeof(char) * size);
-	if (add == NULL)
-	{
-		free(str1);
-		free(str2);
-		printError();
-		return (NULL);
-	}
-	size--, len2--, len1--;
-	sum = k = 0;
-	sum = (str1[len1] - '0') + k;
-	add[size] = (sum % 10) + '0';
-	k = sum / 10;
-	size--, len1--;
-	while (len2 >= 0)
-	{
-		if (len1 < 0)
-			sum = (str2[len2] - '0') + k;
-		else
-			sum = (str1[len1] - '0') + (str2[len2] - '0') + k;
-		add[size] = (sum % 10) + '0';
-		k = sum / 10;
-		size--, len1--, len2--;
-	}
-	if (k >= 0 && k <= 9)
-	{
-		add[size] = k + '0';
-		return (add);
-	}
-	else if (k > 9)
-	{
-		add[size] = (k % 10) + '0';
-		newAdd = _reallocate(add, reSize + 1);
-		if (newAdd == NULL)
-			return (NULL);
-		newAdd[0] = (k / 10) + '0';
-		for (i = 1; i < reSize + 1; i++)
-			newAdd[i] = add[i];
-		free(add);
-		return (newAdd);
-	}
-	add[size] = '0';
-	return (add);
-}
-/**
-* printResult - prints result
-* @result: resultant string
-* Return: nothing
-**/
-void printResult(char *result)
-{
-	int i;
-
-	if (result[0] != '0')
-		_putchar(result[0]);
-
-	for (i = 1; result[i] != '\0'; i++)
-		_putchar(result[i]);
-	_putchar('\n');
-}
-/**
-* main - multiplies two positive numbers and prints result or error
-* @argc: arguement count
-* @argv: poinetr to arguements
-* Return: 0 or exit with 98
-**/
+ * main - multiple two numbers and print the result
+ * @argc: Number of arguments
+ * @argv: Argument strings
+ *
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
-	int zero, len1, len2, counter;
-	char *str1, *str2, *current, *temp, *sum;
+	int *calc;
+	char *final;
+	unsigned int l1, l2, lsum, i, j, ntmp, rolltmp;
 
-	if (argc < 3 || argc > 3)
+	if (argc != 3)
+		_prt("Error"), exit(98);
+	if (_notdigit(argv[1]) || _notdigit(argv[2]))
+		_prt("Error"), exit(98);
+	for (l1 = 0; argv[1][l1]; l1++)
+		;
+	for (l2 = 0; argv[2][l2]; l2++)
+		;
+	lsum = l1 + l2, final = malloc((lsum + 2) * sizeof(*final));
+	calc = _calloc(lsum * lsum, sizeof(int));
+	if (calc == NULL)
+		_prt("Error"), exit(98);
+	rev_(argv[1]), rev_(argv[2]);
+	for (i = 0; i < l1; i++)
 	{
-		printError();
-		exit(98);
+		rolltmp = 0, ntmp = 0;
+		for (j = 0; j < l2; j++)
+		{
+			ntmp = (argv[1][i] - '0') * (argv[2][j] - '0') + rolltmp;
+			calc[i * lsum + j + i] = ntmp % 10, rolltmp = ntmp / 10;
+		}
+		for (; j < l2 + i; j++, rolltmp /= 10)
+			calc[i * lsum + j + i] = rolltmp % 10;
+		while (rolltmp)
+			calc[i * lsum + j + i] = rolltmp % 10, rolltmp /= 10, j++;
 	}
-	if (digit_checker(argv[1]) == -1 || digit_checker(argv[2]) == -1)
-		exit(98);
-	len1 = _strlen(argv[1]);
-	len2 = _strlen(argv[2]);
-	if (len2 > len1)
-	{
-		str1 = argv[2];
-		str2 = argv[1];
-		counter = len1 - 1;
-	}
-	else
-	{
-		str1 = argv[1];
-		str2 = argv[2];
-		counter = len2 - 1;
-	}
-	zero = 0;
-	current = mulNums(str1, str2[counter], zero);
-	if (current == NULL)
-		exit(98);
-	while (--counter >= 0)
-	{
-		temp = mulNums(str1, str2[counter], zero++);
-		if (temp == NULL)
-			exit(98);
-		sum = addNums(current, temp);
-		if (sum == NULL)
-			exit(98);
-		free(current);
-		free(temp);
-		current = sum;
-	}
-	printResult(sum);
+	for (i = 0, rolltmp = 0; i < lsum; i++, rolltmp /= 10)
+		rolltmp += _addup(calc, lsum, i), final[i] = rolltmp % 10 + '0';
+	final[i + 1] = '\0', i = cut_zeros(final), rev_(final);
+	final[i + 2] = '\0', _prt(final), free(calc), free(final);
 	return (0);
 }
