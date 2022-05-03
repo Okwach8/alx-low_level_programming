a011b0d52dfff9549990935886403ecafae006ae
Okwach8 <westher188@gmail.com>
2022-05-03 18:25:00 +0300

    File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,47 +1,59 @@
 #include "main.h"
 /**
- * main - copy the content of one file to another
- * @ac: argument count
- * @av: argument vector
- *
- * Return: 0
- */
-int main(int ac, char **av)
+  * printerrors - just helping me print errors
+  * @message: the thing to print
+  * @file: the file name
+  * @exitVal: the exit status
+  * Return: void
+  */
+void printerrors(char *message, char *file, int exitVal)
 {
-	int fdr, fdw, err, bytes;
-	char buf[BUFFERSIZE];
-	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	dprintf(STDERR_FILENO, "%s%s\n", message, file);
+	exit(exitVal);
+}
+/**
+  * main - copies data from one file to another
+  * @argc: # of args passed
+  * @argv: pointer to array containing args
+  *
+  * Return: 0 for win
+  */
+int main(int argc, char **argv)
+{
+	int fddest, fdsrc, readVal, writeVal;
+	char buffer[1024];
 
-	if (ac != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	if (av[1] == NULL)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	if (av[2] == NULL)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-	fdr = open(av[1], O_RDONLY);
-	if (fdr == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	fdw = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
-	if (fdw == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-	err = bytes = 1;
-	while (bytes)
+	if (argc != 3)
+		printerrors("Usage: cp file_from file_to", "", 97);
+
+	fddest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fddest == -1)
+		printerrors("Error: Can't write to ", argv[2], 99);
+
+	fdsrc = open(argv[1], O_RDONLY);
+	if (fdsrc == -1)
+		printerrors("Error: Can't read from file ", argv[1], 98);
+
+
+	do {
+		readVal = read(fdsrc, buffer, 1024);
+		if (readVal == -1)
+			printerrors("Error: Can't read from file ", argv[1], 98);
+
+		writeVal = write(fddest, buffer, readVal);
+		if (writeVal == -1 || writeVal != readVal)
+			printerrors("Error: Can't write to ", argv[2], 99);
+
+		} while (writeVal == 1024);
+	if (close(fdsrc))
 	{
-		bytes = read(fdr, buf, BUFFERSIZE);
-		if (bytes == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-		if (bytes > 0)
-		{
-			err = write(fdw, buf, bytes);
-			if (err == -1)
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-		}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdsrc);
+		exit(100);
 	}
-	err = close(fdr);
-	if (err == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdr), exit(100);
-	err = close(fdw);
-	if (err == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdw), exit(100);
+	if (close(fddest))
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fddest);
+		exit(100);
+	}
 	return (0);
 }
